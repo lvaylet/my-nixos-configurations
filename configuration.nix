d5505a389a243ec9f245ee2b6c66c61196096a27
Laurent VAYLET <laurent.vaylet@gmail.com>
2026-05-07 11:48:33 +0200

    feat: get closer to LazyVim

--- configuration.nix
+++ configuration.nix
@@ -228,24 +228,27 @@
     enable = true;
     settings = {
       vim = {
-        viAlias = false;
+        viAlias = true;
         vimAlias = true;
 
         theme = {
           enable = true;
-          name = "nord";
-          style = "dark";
-          transparent = true;
+          name = "catppuccin";
+          style = "mocha";
         };
 
-        statusline.lualine.enable = true; # Status line
-        telescope.enable = true; # Fuzzy finder
-        autocomplete.nvim-cmp.enable = true; # Fancy completion menu
+        statusline.lualine.enable = true; # Status line - https://github.com/nvim-lualine/lualine.nvim
+        telescope.enable = true; # Fuzzy finder - https://github.com/nvim-telescope/telescope.nvim
+        autocomplete.nvim-cmp.enable = true; # Completion plugin - https://github.com/hrsh7th/nvim-cmp
+        filetree.nvimTree.enable = true; # File explorer - https://github.com/nvim-tree/nvim-tree.lua
+
+        binds.whichKey.enable = true; # Show available keybindings in a popup as you type.
 
         lsp = {
           enable = true;
           formatOnSave = true;
         };
+
         languages = {
           enableTreesitter = true;
 
