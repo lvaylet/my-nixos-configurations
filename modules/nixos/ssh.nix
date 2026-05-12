3b85997cee7e530d2553686f2b9bca9c3ab15826
Laurent VAYLET <laurent.vaylet@gmail.com>
2026-05-12 22:44:12 +0200

    feat: make SSH more secure

--- modules/nixos/ssh.nix
+++ modules/nixos/ssh.nix
@@ -3,8 +3,10 @@
   services.openssh = {
     enable = true;
     settings = {
-      PermitRootLogin = "no";
-      PasswordAuthentication = false;
+      PermitRootLogin = "no"; # Whether the root user can login using ssh.
+      # Require public key authentication for better security.
+      PasswordAuthentication = false; # Specifies whether password authentication is allowed.
+      KbdInteractiveAuthentication = false; # Specifies whether keyboard-interactive authentication is allowed.
     };
     openFirewall = true;
   };
